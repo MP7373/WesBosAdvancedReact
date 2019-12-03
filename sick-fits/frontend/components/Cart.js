33897cde4b4746aa2d632bc1f9511d39ca0cf86e
Martin <martinpayne73@gmail.com>
2019-12-02 23:08:39 -0500

    completed part 38 (creating frontend cart)

---
+++ sick-fits/frontend/components/Cart.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-unused-expressions */
+import React from 'react';
+import CartStyles from './styles/CartStyles';
+import Supreme from './styles/Supreme';
+import CloseButton from './styles/CloseButton';
+import SickButton from './styles/SickButton';
+
+const Cart = () => (
+  <CartStyles open>
+    <header>
+      <CloseButton title="close">&times;</CloseButton>
+      <Supreme>Your Cart</Supreme>
+      <p>You Have __ Items in your cart.</p>
+    </header>
+    <footer>
+      <p>$23.90</p>
+      <SickButton>Checkout</SickButton>
+    </footer>
+  </CartStyles>
+);
+
+export default Cart;
